8a5cc015ecb533ee65dafd12efeae156e1bbb9ef
Learner <ethical984@gmail.com>
2026-08-01 18:51:20 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,12 +1,33 @@
+======
+TAKE SonarQube.sh to install ======
+
+Expand window to show video
+SETUP: Launch another EC2 instance - Amazon linux 2 - t2.medium is must , it will not run on micro
+============ script
+
+===
+
 #! /bin/bash
+#Launch an instance t2.medium, port 9000
+
+vi sonar.sh 
+paste below commands and save
+
+--start--
+
 cd /opt/
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
 unzip sonarqube-8.9.6.50800.zip
-sudo dnf install java-17-amazon-corretto -y
+sudo dnf install java-21-amazon-corretto -y
 useradd sonar
 chown sonar:sonar sonarqube-8.9.6.50800 -R
 chmod 777 sonarqube-8.9.6.50800 -R
 su - sonar
+
 # use the below command manually after installation
-#sh /opt/sonarqube-8.9.6.50800/bin/linux-x86-64/sonar.sh start
+#sh /opt/sonarqube-8.9.6.50800/bin/linux/sonar.sh start
 #echo "user=admin & password=admin"
+
+--end--
+
+sh sonar.sh
